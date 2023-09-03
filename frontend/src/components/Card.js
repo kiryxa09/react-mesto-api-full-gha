e0e833e019c19000a1b21df190a9597ce6027557
kirikland09 <baibakovkir@yandex.ru>
2023-09-03 11:06:36 +0300

    fixing front v10

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 
 function Card(props) {
   const currentUser = React.useContext(CurrentUserContext);
-  const isOwn = props.card.owner._id === currentUser._id;
+  const isOwn = props.card.owner === currentUser._id;
   const isLiked = props.card.likes.some((i) => i._id === currentUser._id);
   const cardLikeButtonClassName = `element__like ${
     isLiked && "element__like_active"
